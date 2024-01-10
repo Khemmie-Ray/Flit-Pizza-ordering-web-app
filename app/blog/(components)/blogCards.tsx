241e345b2044e8e81309049162c885fbbcfe7a93
Khemmie-Ray <atokemmy@gmail.com>
2024-01-10 18:21:51 +0100

    fixing failed deployment

--- app/blog/(components)/blogCards.tsx
+++ app/blog/(components)/blogCards.tsx
@@ -47,8 +47,8 @@ const BlogCards = () => {
         },
     ]
     
-    const CardBlog = blog.map((info, index) => (<Link href={`blog/${info.id}`}>
-        <div className='flex justify-between w-[90%] mx-auto mb-8 lg:flex-row md:flex-row flex-col' key={index}>
+    const CardBlog = blog.map((info, index) => (<Link href={`blog/${info.id}`} key={index}>
+        <div className='flex justify-between w-[90%] mx-auto mb-8 lg:flex-row md:flex-row flex-col'>
             <div className='bg-cover bg-center h-[30vh]  w-[50%] relative' style={{ backgroundImage: `url(${info.img})` }}>
             <p className='absolute top-4 left-4 font-[700] text-[1.5rem] bg-[rgba(0,0,0,0.8)] p-2 rounded-full px-6 text-white'>{info.date}</p>
             </div>
